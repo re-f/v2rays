b545039f86176d41a0f2108cfd309293955b22e2
Ref <z.ref@outlook.com>
2021-05-17 15:53:44 +0000

    add kill v2ray func

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 	"text/template"
 
@@ -123,4 +125,44 @@ func separator(s string) func() string {
 		}
 		return s
 	}
+}
+
+func killV2ray() error {
+	oldV2rayPid := 0
+	{ // find old v2ray pid
+		pgrepPath, err := exec.LookPath("pgrep")
+		if err != nil {
+			return errors.Wrap(err, "can't find pgrep in $PATH, got error :")
+		}
+
+		v2rayPidRaw := ""
+		if out, err := exec.Command(pgrepPath, "v2ray$").Output(); err != nil {
+			return errors.Wrapf(err, "find old v2ray process got error, and output is %v", v2rayPidRaw)
+		} else {
+			v2rayPidRaw = strings.TrimSpace(string(out))
+		}
+
+		fmt.Printf("pgrep v2ray output: %v\n", v2rayPidRaw)
+
+		if pLen := len(strings.Split(v2rayPidRaw, "\n")); pLen == 0 {
+			fmt.Println("Warning: v2ray not runing before")
+			return nil
+		} else if pLen == 1 {
+			pid, err := strconv.Atoi(v2rayPidRaw)
+			if err != nil {
+				return errors.Wrapf(err, "pgrep v2ray get non-numeric output: %v", v2rayPidRaw)
+			}
+			oldV2rayPid = pid
+		} else {
+			return errors.New(fmt.Sprintf("expect 1 v2ray process, but got %v", pLen))
+		}
+	}
+
+	fmt.Printf("PID: %d, Name: v2ray will be killed.\n", oldV2rayPid)
+	proc, err := os.FindProcess(oldV2rayPid)
+	if err != nil {
+		return errors.Wrap(err, "find v2ray(%v) got error: ")
+	}
+	// Kill the process
+	return proc.Kill()
 }
